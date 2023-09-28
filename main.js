19ad445c1c78181252f860cc3ac571d94db661e7
RagipatiRafi <146105040+RagipatiRafi@users.noreply.github.com>
2023-09-28 10:46:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+var a="name";
+function varExampleSample(){
+        if(true){
+            
+            console.log(a);
+            a="newname";
+            console.log(a);
+            a="secondnewname"
+        }
+        console.log(a);
+    
+}
+varExampleSample();
+
+function letExample(){
+    if(true){
+        let b="place";
+        console.log(b);
+        b="newplace";
+        console.log(b);
+        b="Secondnewplace"
+        console.log(b);
+    }
+}
+letExample();
+
+function constExample(){
+    const x="thing";
+    console.log(x);
+   y="newthing";
+    console.log(y);
+}
+constExample();
+
+
+
+
+
+
+
+
+
